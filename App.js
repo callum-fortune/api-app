c50fc40944769eb5b8c26f77a9f8797322530d8a
Callum Fortune <callumfortune03@outlook.com>
2024-05-06 02:12:41 +0100

    Revert "react native working with tailwind"
    This reverts commit 2dea49a930e53bb9207e1ba2cd5f46910d2a51c2.

--- App.js
+++ App.js
@@ -3,11 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex h-screen items-center justify-center">
-
-      <Text className="bg-red-400 px-4 py-2 text-white">Hello, world!</Text>
-      
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-};
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
