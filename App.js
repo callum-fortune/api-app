3ce591371557807cef47ffcfd0959262da2f23c4
Callum Fortune <callumfortune03@outlook.com>
2024-05-06 02:16:46 +0100

    fix shit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View className="flex h-screen items-center justify-center">
 
-      <Text className="bg-red-400 px-4 py-2 text-white">Hello, world!</Text>
+      <Text className="bg-black px-4 py-2 text-white">Hello, world!</Text>
       
       <StatusBar style="auto" />
     </View>
